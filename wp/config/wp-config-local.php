e497191d41f3517439beea0fc09540127e5b1056
Juergen Arne Klein <mail@juergen-arne-klein.de>
2022-03-08 20:53:17 +0100

    [env] update local env

--- wp/config/wp-config-local.php
+++ wp/config/wp-config-local.php
@@ -131,6 +131,8 @@ define( 'PATH_CURRENT_SITE', '/' );
 define( 'SITE_ID_CURRENT_SITE', 1 );
 define( 'BLOG_ID_CURRENT_SITE', 1 );
 
+define('WP_ALLOW_REPAIR', true);
+
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
